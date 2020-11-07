ead40355953708dc77392604c0c0354ab5af3519
nif33 <jennifer_dai2000@yahoo.ca>
2020-11-07 15:16:58 -0800

    change button width to be specific to menu buttons, change font style

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -12,12 +12,12 @@
       <div id="menu">
         <v-col>
           <v-row justify="end" class="py-2">
-            <v-btn color="accent">Play</v-btn>
-            <v-btn color="muted_accent light--text">Scores</v-btn>
+            <v-btn class="accent menu-btn">Play</v-btn>
+            <v-btn class="muted_accent light--text menu-btn">Scores</v-btn>
           </v-row>
           <v-row justify="end" class="py-2">
-            <v-btn color="muted_accent light--text" @click="about ^= true">About</v-btn>
-            <v-btn color="muted_accent light--text">Help</v-btn>
+            <v-btn class="muted_accent light--text menu-btn" @click="about ^= true">About</v-btn>
+            <v-btn class="muted_accent light--text menu-btn">Help</v-btn>
           </v-row>
         </v-col>
       </div>
