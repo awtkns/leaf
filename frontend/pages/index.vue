384efb8345e712764f233fd3500c4f9192982853
Adam <cadamrun@gmail.com>
2020-11-07 11:01:39 -0800

    simple page

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    Index
+    <h1>APP TITLE</h1>
+    <h2>SFU</h2>
+    <v-row>
+      <v-col v-for="c in choices" sm="6" md="3">
+        <v-card>
+          <h2>{{c}}</h2>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 export default {
-    name: "index",
-    data: () => ({})
+  name: "index",
+  data: () => ({
+    acronym: "SFU",
+    choices: ["A", "B", "C", "D"]
+  })
 }
 </script>
