cd5b4fb7c835f9cb9a6ce538fdfa51bb9479afd5
Asim Shrestha <asim.shrestha@hotmail.com>
2020-11-07 14:01:28 -0800

    feat: add room joining functionality

--- backend/api/sockets/index.py
+++ backend/api/sockets/index.py
@@ -1,24 +1,27 @@
 from . import sio
 from ..routes import generate
 
-# Overview, client connects when the open the webpage
+# Overview:
+# Client connects when they open the webpage
 # Client emits a join_room event when the click play
 # join_room will link the client to a room on the socket
 # When the game has enough people to start / is forced to start
 #	start_round data will be emitted to all clients within the room
+# Once everyone has returned answers, results will be emitted, wait, then the next round emitted
+
 
 @sio.event
 def connect(sid, environ):
-    print('connect ', sid)
+	print('connect ', sid)
 
 
 @sio.event
 async def join_room(sid, data):
 	# Get connection information
 	name = 'testName' # TODO get name from data
-	# TODO generate new room if needed, use just one room for now
+	# TODO generate new room if needed, use one room for now
 	room = 'test'
-	print('vals', name, room, sid)
+
 	# Save connection information
 	await sio.save_session(sid, {
 		'name': name,
@@ -29,28 +32,30 @@ async def join_room(sid, data):
 	return "OK", "Joined room"
 
 
-async def start_game():
-    # data = get_round_data()
-    # sio.emit('round_start', data, room= get room value here)
-    pass
-
-
 @sio.event
-async def start_round(sid, data):
+async def start_game(sid, data):
+	await start_round()
+	return "OK"
 
-    # sio.enter_room(sid, 'chat_users')
-    # store room value somewhere?
-	payload = await generate.return_acronyms()
-	return "OK", payload
 
-# Send info back with: sio.emit('my event', {'data': 'foobar'})
+async def start_round():
+	data = await generate.return_acronyms()
+	await sio.emit('round_start', data, room='test') # TODO add room
+
 
 @sio.event
 async def send_answer(sid, data):
-	answer = data.answer
-	is_correct = True # TODO validate with backend first
-	# If everyone has finished {return results}
+	# Validate answer
+	answer = data['answer']
+	is_correct = True # TODO validate answer with backend
+
+	# Start new round if needed
+	# TODO only start new round / return results if everyone has finished
+	await start_round()
+
+	# TODO only return results if everyone has finished
 	return "OK", is_correct
+
 
 @sio.event
 def message(sid, data):
@@ -60,4 +65,4 @@ def message(sid, data):
 
 @sio.event
 def disconnect(sid):
-    print('disconnect ', sid)
+	print('disconnect ', sid)
--- frontend/components/WebSocketTest.vue
+++ frontend/components/WebSocketTest.vue
@@ -7,6 +7,12 @@
 	 <v-btn @click="join">
       join game
     </v-btn>
+	 <v-btn @click="startGame">
+      Start game
+    </v-btn>
+	 <v-btn @click="sendAnswer">
+      Send answer
+    </v-btn>
     {{ messageRxd }}
   </div>
 </template>
@@ -38,11 +44,25 @@ export default {
         this.messageRxd.push(data)
       })
 	 },
+	 startGame() {
+      this.socket.emit('start_game', {data: 'hello from nuxt'}, (resp, data) => {
+        console.log(resp, data)
+      })
+	 },
+	 sendAnswer() {
+      this.socket.emit('send_answer', {answer: 'fake'}, (resp, data) => {
+        console.log(resp, data)
+      })
+	 },
+	 
 
 	 
   },
   mounted() {
-    this.socket = this.$nuxtSocket({path: '/ws/socket.io'})
+	 this.socket = this.$nuxtSocket({path: '/ws/socket.io'})
+	 this.socket.on('round_start', (msg, cb) => {
+		 this.messageRxd.push("RECIEVED ROUND START", msg)
+	 })
   }
 }
 </script>
