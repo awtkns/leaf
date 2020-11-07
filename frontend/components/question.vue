d3dc39c10cb7e42a396d427f3b5d27aea540ab9c
nif33 <jennifer_dai2000@yahoo.ca>
2020-11-07 12:57:33 -0800

    add page and components for game

---
+++ frontend/components/question.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>What is {{acronym}}?</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      acronym: "LEAF"
+    }
+  }
+}
+</script>
---
+++ frontend/pages/game.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <question />
+    <choices />
+    <score-time />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "game"
+}
+</script>
