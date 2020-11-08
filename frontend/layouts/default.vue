a3d4d02ee68b584c791cd3b0c95983f765353eb2
Adam <cadamrun@gmail.com>
2020-11-07 16:34:51 -0800

    added leaf background

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,24 @@
 <template>
-  <v-app style="background-color: #398840">
-    <v-main>
-      <v-container style="background-color: #e6e6e6; padding-left:0%; padding-right: 3%">
-        <nuxt />
-      </v-container>
+  <v-app style="background-color: #e6e6e6">
+    <v-main id="leaf">
+      <nuxt />
+
+      <img src="../assets/title-image.svg" style="position: fixed; bottom: 20px; left: -80px; max-height: 500px"/>
     </v-main>
   </v-app>
 </template>
+
+<style>
+  #leaf {
+    background-image: url("../assets/home.svg");
+
+    /*background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)) , url("../static/raven.jpg") center center;*/
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+  }
+</style>
+
