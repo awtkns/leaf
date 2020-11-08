483c6e8063a53905584cec089ecee2fe79b37e21
Ethan Haid <ehaid@sfu.ca>
2020-11-07 18:06:37 -0800

    update requirements.txt

--- backend/api/routes/generate.py
+++ backend/api/routes/generate.py
@@ -11,27 +11,15 @@ router = APIRouter()
 
 @router.get('/single')
 async def get_single_meaning(acronym: str = 'SFU'):
-    """ Returns a single word corresponding to and acronym"""
+    """ Returns a single phrase corresponding to an acronym"""
 
-    phrases = ["", "", ""]
-    for c in acronym:
-        words = []
-        
-        async for wordData in db.client.unigrams.get_collection(c.lower()).find():
-            words.append(wordData['word'])
-        
-        for i in range(3):
-            phrases[i] += random.choice(words) + " "
-    
-    return phrases
-
+    return generate_helper.generate_random_acronyms(acronym = acronym, numPhrases = 1)
 
 @router.get('/')
 async def return_acronyms():
     valid_pair = generate_helper.build_valid_pair()
 
-    # TODO: delete dummy list
-    random_phrases = ['decoy_1','decoy_2','decoy_3']
+    random_phrases = generate_helper.generate_random_acronyms(acronym = valid_pair['phrases'], numPhrases = 3)
     
     generate_helper.build_payload(valid_pair, random_phrases)
 
