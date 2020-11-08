52c1ee7538296595b852ebe9afb6185fa845bac5
nif33 <jennifer_dai2000@yahoo.ca>
2020-11-07 19:09:15 -0800

    remove score button

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,22 +4,19 @@
       <h2 class="text-right">{{ preTitle }}</h2>
       <h1 class="text-right overlay" color="primary">{{ postTitle }}</h1>
     </div>
-    <v-row>
-      <v-col>
       <div id="menu">
         <v-col>
           <v-row justify="end" class="py-2">
             <v-btn class="accent menu-btn" @click="playGame">Play</v-btn>
-            <v-btn class="muted_accent light--text menu-btn">Scores</v-btn>
           </v-row>
           <v-row justify="end" class="py-2">
             <v-btn class="muted_accent light--text menu-btn" @click="about ^= true">About</v-btn>
+          </v-row>
+          <v-row justify="end" class="py-2">
             <v-btn class="muted_accent light--text menu-btn" @click="help ^= true">Help</v-btn>
           </v-row>
         </v-col>
       </div>
-      </v-col>
-    </v-row>
 
     <About v-if="about" @closeAbout="about = false" style="position: fixed; bottom: 0; right: 0;"/>
     <Help v-if="help" @closeHelp="help = false" style="position: fixed; bottom: 0; right: 0;"/>
@@ -60,13 +57,12 @@ export default {
   methods: {
     playGame() {
       this.$store.commit('setName', this.name);
-
       if (!this.$store.state.name) {
         this.nameDialog = true
       } else {
         this.showGame = true
       }
-    }
+    },
   }
 }
 </script>
