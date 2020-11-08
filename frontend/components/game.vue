588c8298896c09bf6342f6acbcf48ea5cf71720c
Asim Shrestha <asim.shrestha@hotmail.com>
2020-11-07 16:43:34 -0800

    feat: fix button disable

--- frontend/components/game.vue
+++ frontend/components/game.vue
@@ -3,7 +3,7 @@
     <v-card-title>What is {{ acronym }}?</v-card-title>
     <v-card-subtitle v-if="timeLeft">Ending in {{ timeLeft }}</v-card-subtitle>
     <v-card-text>
-      <v-btn v-for="w in words"  color="accent" @click="sendAnswer(w)" :disabled="is_correct != undefined">
+      <v-btn v-for="w in words"  color="accent" @click="sendAnswer(w)" :disabled="answer_submitted == true">
         {{ w }}
       </v-btn>
     </v-card-text>
@@ -47,6 +47,7 @@ export default {
   },
   methods: {
     reset() {
+      this.answer_submitted = false
       this.is_correct = undefined
       clearInterval(this.timer)
     },
@@ -58,6 +59,7 @@ export default {
       })
     },
     sendAnswer(answer) {
+		this.answer_submitted = true
       this.socket.emit('send_answer', {answer: answer}, (resp, data) => {
         this.is_correct = data
       })
